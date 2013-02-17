be3be94d05fa8b762d64d12ac2a1420d30fb07cc
Krzysztof Chomicki <khalosh@gmail.com>
2013-02-17 17:49:08 +0100

    Generacja aplikacji

---
+++ plytospis/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Plytospis::Application.config.secret_token = '930c253bbe657c8e69c4833e6829dfbdbf581a946573c10f07dab580c016c41e67559b7a8e324ab89557e93f93c525d9c1b023ece16ca07580a26874eb2de6d5'
